fd3de0c24dabc9bc76d61ad7e9a8d3e4cd6d698a
BYTRONICS <BYTRONICS>
2017-02-08 15:26:01 +0300

    The find_max_min commit

---
+++ find_max_min.py
@@ -0,0 +1,10 @@
+def find_max_min(x):
+    if min(x) == max(x):
+      return [len(x)]
+    else:
+      return [min(x), max(x)]
+def find_max_min(x):
+    if min(x) == max(x):
+      return [len(x)]
+    else:
+      return [min(x), max(x)]
