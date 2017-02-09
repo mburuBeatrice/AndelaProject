304ff5e6d76a02eef9596ca72c8831510f0ebc7c
BYTRONICS <BYTRONICS>
2017-02-09 17:13:01 +0300

    missing number solution

---
+++ missing_number.py
@@ -0,0 +1,9 @@
+def find_missing(array1,array2):
+    if array1 == [] or array2 == [] or array1 == array2:
+        return 0
+    for x in array1:
+        if x not in array2:
+            return x
+    for y in array2:
+        if y not in array1:
+            return y
